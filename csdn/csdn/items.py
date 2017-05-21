b3cb44512f88a0f8f5f21cfc3208dc772f444347
leo019527 <15281070@bjtu.edu.cn>
2017-05-21 20:22:37 +0800

    修改了items.py文件，定义了需要爬取的信息

--- csdn/csdn/items.py
+++ csdn/csdn/items.py
@@ -8,7 +8,33 @@
 import scrapy
 
 
-class CsdnItem(scrapy.Item):
+class CsdniosItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+	title = scrapy.Field()
+	href = scrapy.Field()
+
+
+class TieziItem(scrapy.Item):
+	startTime = scrapy.Field()
+	tag = scrapy.Field()
+	title = scrapy.Field()
+	top = scrapy.Field()
+	recommend = scrapy.Field()
+	closeTime = scrapy.Field()
+	content = scrapy.Field()
+	user = scrapy.Field()
+	like = scrapy.Field()
+	dislike = scrapy.Field()
+	guanzhu = scrapy.Field()
+	shoucang = scrapy.Field()
+	plate = scrapy.Field()
+
+class yonghuItem(scrapy.Item):
+	level = scrapy.Field()
+	postNum = scrapy.Field()
+	createTime = scrapy.Field()
+	user = scrapy.Field()
+	concerns = scrapy.Field()
+	beConcerneds = scrapy.Field()
+	updateTimes = scrapy.Field()
