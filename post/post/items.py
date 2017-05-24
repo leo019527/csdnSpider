0531556e239afb04242a90c93b659784621cdbaf
leo019527 <15281070@bjtu.edu.cn>
2017-05-24 21:34:29 +0800

    爬了title，starttime，tag

--- post/post/items.py
+++ post/post/items.py
@@ -11,4 +11,17 @@ import scrapy
 class PostItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    starttime = scrapy.Field()
+    tag = scrapy.Field()
+    title = scrapy.Field()
+    istop = scrapy.Field()
+    recommend = scrapy.Field()
+    stoptime = scrapy.Field()
+    content = scrapy.Field()
+    usr = scrapy.Field()
+    like = scrapy.Field()
+    dislike = scrapy.Field()
+    focus = scrapy.Field()
+    collect = scrapy.Field()
+    bankuai = scrapy.Field()
     pass
